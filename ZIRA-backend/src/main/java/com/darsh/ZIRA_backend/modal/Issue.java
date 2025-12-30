d19e6d2a2cdd45b961f391e9bd9ce4ba51a07b5b
Darsh_Balar <darshbalar2005@gmail.com>
2025-12-30 15:31:04 +0530

    Created Initial Modals

---
+++ ZIRA-backend/src/main/java/com/darsh/ZIRA_backend/modal/Issue.java
@@ -0,0 +1,16 @@
+package com.darsh.ZIRA_backend.modal;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Issue {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
---
+++ ZIRA-backend/src/main/java/com/darsh/ZIRA_backend/modal/User.java
@@ -0,0 +1,25 @@
+package com.darsh.ZIRA_backend.modal;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+    private String email;
+    private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "assignee",cascade = CascadeType.ALL)
+    private List<Issue> assignedIssues;
+
+    private int projectSize;
+}
