f19835f66f96a7e27f1093ad57c3411eefea8e4a
Darsh_Balar <darshbalar2005@gmail.com>
2025-12-30 14:36:35 +0530

    Initial Spring Boot Config

---
+++ ZIRA-backend/src/main/java/com/darsh/ZIRA_backend/ZiraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.darsh.ZIRA_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZiraBackendApplication.class, args);
+	}
+
+}
