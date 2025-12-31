2dbf7d84a17066859d31852bbe7f260ad7fc65a0
Darsh_Balar <darshbalar2005@gmail.com>
2025-12-31 16:51:18 +0530

    Created Project Entity

---
+++ ZIRA-backend/src/main/java/com/darsh/ZIRA_backend/modal/Project.java
@@ -0,0 +1,40 @@
+package com.darsh.ZIRA_backend.modal;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.sun.jdi.PrimitiveValue;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private String category;
+
+    private List<String> tags;
+
+    @JsonIgnore
+    @OneToOne(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Chat chat;
+
+    @ManyToOne
+    private User owner;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Issue> issues;
+
+    @ManyToMany
+    private List<User> team;
+
+}
