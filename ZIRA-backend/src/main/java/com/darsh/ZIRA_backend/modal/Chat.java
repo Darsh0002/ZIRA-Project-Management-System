a822681438d80c38f4a037dcab83353236ab15e8
Darsh_Balar <darshbalar2005@gmail.com>
2026-01-02 21:14:26 +0530

    Completed Chat Entity

--- ZIRA-backend/src/main/java/com/darsh/ZIRA_backend/modal/Chat.java
+++ ZIRA-backend/src/main/java/com/darsh/ZIRA_backend/modal/Chat.java
@@ -1,9 +1,11 @@
 package com.darsh.ZIRA_backend.modal;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Entity
 @Data
@@ -14,6 +16,15 @@ public class Chat {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+
     @OneToOne
     private Project project;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "chat", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Message> messages;
+
+    @ManyToMany
+    private List<User> users;
 }
