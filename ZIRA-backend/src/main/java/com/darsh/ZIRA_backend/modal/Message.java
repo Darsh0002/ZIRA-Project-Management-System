5ebd31b9da249ca2648f6ac06f071f842c35a3c1
Darsh_Balar <darshbalar2005@gmail.com>
2026-01-02 21:15:48 +0530

    Created Message Entity

---
+++ ZIRA-backend/src/main/java/com/darsh/ZIRA_backend/modal/Message.java
@@ -0,0 +1,27 @@
+package com.darsh.ZIRA_backend.modal;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    private Chat chat;
+
+    @ManyToOne
+    private User sender;
+}
