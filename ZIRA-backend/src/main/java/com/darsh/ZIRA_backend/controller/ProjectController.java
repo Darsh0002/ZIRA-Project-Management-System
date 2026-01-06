b51c5f1b5489db7be555e7e0b60d08e4cb07e287
Darsh_Balar <darshbalar2005@gmail.com>
2026-01-06 22:06:27 +0530

    Created Project Controller

---
+++ ZIRA-backend/src/main/java/com/darsh/ZIRA_backend/controller/ProjectController.java
@@ -0,0 +1,35 @@
+package com.darsh.ZIRA_backend.controller;
+
+import com.darsh.ZIRA_backend.modal.Project;
+import com.darsh.ZIRA_backend.modal.User;
+import com.darsh.ZIRA_backend.service.ProjectService;
+import com.darsh.ZIRA_backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+@CrossOrigin("*")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<Project>> getProjects(
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String tag,
+            @RequestHeader("Authorization") String jwt
+    ) throws Exception {
+        User user = userService.findUserProfileByJwt(jwt);
+        List<Project> projects = projectService.getProjectByTeam(user, category, tag);
+        return new ResponseEntity<>(projects, HttpStatus.OK);
+    }
+}
