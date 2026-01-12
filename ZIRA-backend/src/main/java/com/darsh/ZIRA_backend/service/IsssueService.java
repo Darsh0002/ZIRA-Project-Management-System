33fe645e71e097bd8ee3ffcc79fa4a88ea46495d
Darsh_Balar <darshbalar2005@gmail.com>
2026-01-12 23:13:50 +0530

    Created Method For Creating a Issue

--- ZIRA-backend/src/main/java/com/darsh/ZIRA_backend/service/IsssueService.java
+++ ZIRA-backend/src/main/java/com/darsh/ZIRA_backend/service/IsssueService.java
@@ -1,6 +1,9 @@
 package com.darsh.ZIRA_backend.service;
 
+import com.darsh.ZIRA_backend.dto.IssueRequest;
 import com.darsh.ZIRA_backend.modal.Issue;
+import com.darsh.ZIRA_backend.modal.Project;
+import com.darsh.ZIRA_backend.modal.User;
 import com.darsh.ZIRA_backend.repository.IssueRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,12 +15,30 @@ public class IsssueService {
     @Autowired
     private IssueRepo issueRepo;
 
+    @Autowired
+    private ProjectService projectService;
+
     public Issue getIssueById(Long issueId) throws Exception {
         return issueRepo.findById(issueId)
                 .orElseThrow(() -> new RuntimeException("No Issue Found With Id: " + issueId));
     }
 
-    public List<Issue> getIssueByProjectId(Long projectId){
+    public List<Issue> getIssueByProjectId(Long projectId) {
         return issueRepo.findByProjectId(projectId);
+    }
+
+    public Issue createIssue(IssueRequest issueRequest, User user) throws Exception {
+        Project project = projectService.getProjectById(issueRequest.getProjectId());
+
+        Issue issue = new Issue();
+        issue.setTitle(issueRequest.getTitle());
+        issue.setDescription(issueRequest.getDescription());
+        issue.setStatus(issueRequest.getStatus());
+        issue.setProject(project);
+        issue.setProjectId(issue.getProjectId());
+        issue.setPriority(issue.getPriority());
+        issue.setDueDate(issueRequest.getDueDate());
+
+        return issueRepo.save(issue);
     }
 }
