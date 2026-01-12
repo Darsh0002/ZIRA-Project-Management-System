20abb4b6934a62982a4c2793ac7d3b706d1c6076
Darsh_Balar <darshbalar2005@gmail.com>
2026-01-12 21:37:27 +0530

    Implemented Java Mail Service

---
+++ ZIRA-backend/src/main/java/com/darsh/ZIRA_backend/service/EmailService.java
@@ -0,0 +1,34 @@
+package com.darsh.ZIRA_backend.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSendException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmailWithToken(String userEmail, String link) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+
+        String subject = "Join Project Team Invitation";
+        String text = "Click the link to join the project team: " + link;
+        helper.setSubject(subject);
+        helper.setText(text, true);
+        helper.setTo(userEmail);
+
+        try {
+            mailSender.send(mimeMessage);
+        } catch (MailSendException e) {
+            throw new MailSendException("Failed to send Email");
+        }
+    }
+}
